d1fa59025d62a6ea7889ac2cd555eb44d523c6ce
Bence A. Tóth <tothab@gmail.com>
2022-09-29 13:40:04 +0200

    Created Dino character

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
-const App = () => <div></div>;
+import "./App.css";
+
+import DinoIdle from "./assets/dino-idle.png";
+import DinoRunning from "./assets/dino-running.png";
+import DinoJumping from "./assets/dino-jumping.png";
+
+const App = () => (
+  <div>
+    <div className="dino idle">
+      <div>
+        <img src={DinoIdle} alt="" />
+      </div>
+    </div>
+    <div className="dino running">
+      <div>
+        <img src={DinoRunning} alt="" />
+      </div>
+    </div>
+    <div className="dino jumping">
+      <div>
+        <img src={DinoJumping} alt="" />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
