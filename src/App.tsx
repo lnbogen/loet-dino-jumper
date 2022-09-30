62191049853d940aea46ba01877073cd7b558cb1
Bence A. Tóth <tothab@gmail.com>
2022-09-30 11:59:56 +0200

    Added hills

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import WorldElement from "./WorldElement";
 import useDinoState from "./useDinoState";
 
 import GroundTexture from "./assets/ground.png";
+import Hills1 from "./assets/hills-1.png";
+import Hills2 from "./assets/hills-2.png";
+import Hills3 from "./assets/hills-3.png";
+import Volcano from "./assets/volcano.png";
 
 import "./App.css";
 
@@ -12,6 +16,30 @@ const App = () => {
   return (
     <div className="game-canvas">
       <div className="sky">
+        <WorldElement
+          speed={["running", "jumping"].includes(dinoState) ? 0.5 : 0}
+          tileWidth={1000}
+          tileHeight={697}
+          tileUrl={Volcano}
+        />
+        <WorldElement
+          speed={["running", "jumping"].includes(dinoState) ? 1 : 0}
+          tileWidth={800}
+          tileHeight={340}
+          tileUrl={Hills3}
+        />
+        <WorldElement
+          speed={["running", "jumping"].includes(dinoState) ? 1.25 : 0}
+          tileWidth={500}
+          tileHeight={196}
+          tileUrl={Hills2}
+        />
+        <WorldElement
+          speed={["running", "jumping"].includes(dinoState) ? 2 : 0}
+          tileWidth={400}
+          tileHeight={91}
+          tileUrl={Hills1}
+        />
         <Dino
           state={dinoState}
           position={dinoPosition}
